98b372875635cd9861bacd0c36ea9ccb7f8e4142
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-05-08 12:29:17 +0200

    format text in list

--- app/src/main/java/dev/kalbarczyk/virtualjournal/ui/view/EntryListScreen.kt
+++ app/src/main/java/dev/kalbarczyk/virtualjournal/ui/view/EntryListScreen.kt
@@ -25,8 +25,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import androidx.compose.ui.unit.sp
 import dev.kalbarczyk.virtualjournal.model.JournalEntry
 import dev.kalbarczyk.virtualjournal.model.previewData
@@ -73,7 +75,9 @@ fun EntryItemView(entry: JournalEntry) {
                 text = entry.content,
                 style = MaterialTheme.typography.bodyMedium,
                 color = MaterialTheme.colorScheme.onSurface,
-                fontSize = 18.sp
+                fontSize = 18.sp,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
             )
 
             Spacer(modifier = Modifier.height(4.dp))
@@ -84,7 +88,9 @@ fun EntryItemView(entry: JournalEntry) {
                     style = MaterialTheme.typography.bodySmall,
                     color = MaterialTheme.colorScheme.onSurfaceVariant,
                      fontWeight = FontWeight.Bold,
-                    fontSize = 18.sp
+                    fontSize = 18.sp,
+                     maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
                 )
             }
         }
